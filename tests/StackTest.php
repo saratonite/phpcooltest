57ce8cf5ff8e728062c214f348ad4ef681b2f21a
Sarath <sarath.hacks@gmail.com>
2015-12-16 22:25:51 +0530

    Added stack test pop and top

--- tests/StackTest.php
+++ tests/StackTest.php
@@ -33,6 +33,23 @@ class StackTest extends PHPUnit_Framework_TestCase {
 		$this->assertTrue($this->stk->push('One'));
 		$this->assertEquals(1,$this->stk->getSize());
 	}
+    
+    public function testStackPop(){
+        $this->stk->push('One');
+        $this->stk->push('Two');
+        $this->stk->push('Three');
+        
+        $this->assertEquals(3,$this->stk->getSize());
+        $this->assertTrue($this->stk->pop());
+        $this->assertEquals(2,$this->stk->getSize());
+    }
+    
+    public function testTop(){
+        $this->stk->push('One');
+        $this->stk->push('Two');
+        $this->stk->push('Three');
+        $this->assertEquals('Three',$this->stk->top());
+    }
 
 	
 	
